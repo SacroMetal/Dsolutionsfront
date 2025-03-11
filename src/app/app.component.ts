ef8ae24d4b502e5cc541aaa53dffbafeb871eaf7
Jeremy Hidalgo <jeremy.hidalgo@chilepasajes.cl>
2025-03-11 10:22:11 -0300

    inicial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,121 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams  } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+declare const Swal: any;
+
+interface ApiResponse {
+  data: number;
+  message: string;
+}
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Dsolutionsfront';
+export class AppComponent implements OnInit {
+  usuarios: any[] = [];
+  modalAbierto = false;
+
+  nuevoUsuario = {
+    name: '',
+    email: '',
+    edad: null
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.obtenerUsuarios();
+  }
+
+  obtenerUsuarios() {
+    this.http.get<any[]>('http://localhost:8080/listarUsuarios')
+      .subscribe(
+        (data) => {
+          this.usuarios = data;
+        },
+        (error) => {
+          console.error('Error al obtener usuarios:', error);
+        }
+      );
+  }
+
+  abrirModal() {
+    this.modalAbierto = true;
+  }
+
+  cerrarModal() {
+    this.modalAbierto = false;
+  }
+
+  agregarUsuario() {
+    const nameValidate  = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+    const emailValidate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const edadValidate  = /^[0-9]+$/;
+
+    if (!this.nuevoUsuario.name || !nameValidate.test(this.nuevoUsuario.name)) {
+      Swal.fire({
+        icon: "error",
+        title: "Error...",
+        text: "Nombre inválido: solo debe contener letras y espacios!"
+      });
+      return;
+    }
+
+    if (!this.nuevoUsuario.email || !emailValidate.test(this.nuevoUsuario.email)) {
+      Swal.fire({
+        icon: "error",
+        title: "Error...",
+        text: "Correo inválido: debe tener un formato válido!"
+      });
+      return;
+    }
+
+    if (!this.nuevoUsuario.edad || !edadValidate.test(String(this.nuevoUsuario.edad))) {
+      Swal.fire({
+        icon: "error",
+        title: "Error...",
+        text: "Edad inválida: debe contener solo números!"
+      });
+      return;
+    }
+
+    this.http.post<ApiResponse>('http://localhost:8080/crearUsuarios', new HttpParams()
+    .set('name', this.nuevoUsuario.name)
+    .set('email', this.nuevoUsuario.email)
+    .set('edad', this.nuevoUsuario.edad), {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    })
+    .subscribe(
+      (response) => {
+        if (response.data === 1) {
+          Swal.fire({
+            icon: "success",
+            title: "Usuario agregado",
+            text: response.message
+          });
+
+          this.usuarios.push({ ...this.nuevoUsuario, id: this.usuarios.length + 1 });
+          this.cerrarModal();
+          this.nuevoUsuario = { name: '', email: '', edad: null };
+        }
+
+      },
+      (error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Error...",
+          text: 'Error al agregar usuario:', error
+        });
+        return;
+      }
+    );
+
+  }
 }
